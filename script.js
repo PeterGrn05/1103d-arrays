2107a460d984b3ec933f823f28a1f92d8e76eaf2
PeterGrn05 <144786911+PeterGrn05@users.noreply.github.com>
2023-11-10 09:03:13 +0000

    All Done!

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 // задача №1
-const fill = (arraySize, value) => {
-  // решение здесь
+function fill(arraySize, value) {
+  const newArray = [];
+  for (let i = 0; i < arraySize; i++) {
+    newArray.push(value);
+  }
+  return newArray;
 }
 
-const data = 3;
+const val = 3;
 const valueToFill = 'a';
-console.log(fill(data, valueToFill))
+console.log(fill(val, valueToFill))
 
 // задача №2
 const reverse = (array) => {
-  // решение здесь
- }
+  const backwards = [];
+  for (let i = array.length - 1; i >= 0; i -= 1) {
+    backwards.push(array[i]);
+  }
+  return backwards;
+};
 
-const data = [1, 2, 3];
-console.log(reverse(data)); // [3, 2, 1]
+const nums = [1, 2, 3];
+console.log(reverse(nums)); // [3, 2, 1]
 
 // задача №3
-const compact = (array) => {
-  // решение здесь
-}
+function compact(array) {
+  return array.filter(Boolean);
+  }
 
 const data = [0, 1, false, 2, undefined, '', 3, null];
 console.log(compact(data)) // [1, 2, 3]
